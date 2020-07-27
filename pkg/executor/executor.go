71410e3b4b08e5040fdbcccc515508f94d41a5a6
Daniele Rondina <geaaru@sabayonlinux.org>
2020-07-28 00:04:47 +0200

    executor: Add support for LXD profiles

--- pkg/executor/executor.go
+++ pkg/executor/executor.go
@@ -34,6 +34,7 @@ import (
 	"strings"
 
 	helpers "github.com/MottainaiCI/lxd-compose/pkg/helpers"
+	specs "github.com/MottainaiCI/lxd-compose/pkg/specs"
 
 	"golang.org/x/sys/unix"
 
@@ -420,4 +421,50 @@ func (e *LxdCExecutor) CleanUpContainer(containerName string) error {
 	}
 
 	return nil
+}
+
+func (e *LxdCExecutor) GetProfilesList() ([]string, error) {
+	return e.LxdClient.GetProfileNames()
+}
+
+func (e *LxdCExecutor) IsPresentProfile(profileName string) (bool, error) {
+	ans := false
+	list, err := e.GetProfilesList()
+
+	if err != nil {
+		return false, err
+	}
+
+	for _, p := range list {
+		if p == profileName {
+			ans = true
+			break
+		}
+	}
+
+	return ans, nil
+}
+
+func (e *LxdCExecutor) CreateProfile(profile specs.LxdCProfile) error {
+
+	if profile.Name == "" {
+		return errors.New("Invalid profile with empty name")
+	}
+
+	lxdProfile := lxd_api.ProfilesPost{
+		ProfilePut: lxd_api.ProfilePut{
+			Config:  profile.Config,
+			Devices: profile.Devices,
+		},
+		Name: profile.Name,
+	}
+
+	if lxdProfile.ProfilePut.Config == nil {
+		lxdProfile.ProfilePut.Config = make(map[string]string, 0)
+	}
+	if lxdProfile.ProfilePut.Devices == nil {
+		lxdProfile.ProfilePut.Devices = make(map[string]map[string]string, 0)
+	}
+
+	return e.LxdClient.CreateProfile(lxdProfile)
 }
