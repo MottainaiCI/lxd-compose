5f504eab469edf53f898a0b1c38a2d8cca4e088f
Daniele Rondina <geaaru@sabayonlinux.org>
2020-08-03 20:39:22 +0200

    Add logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,213 @@
+/*
+
+Copyright (C) 2020  Daniele Rondina <geaaru@sabayonlinux.org>
+Credits goes also to Gogs authors, some code portions and re-implemented design
+are also coming from the Gogs project, which is using the go-macaron framework
+and was really source of ispiration. Kudos to them!
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Based on the code of the project: https://github.com/mudler/luet
+
+*/
+
+package logger
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	specs "github.com/MottainaiCI/lxd-compose/pkg/specs"
+
+	"github.com/kyokomi/emoji"
+	"github.com/logrusorgru/aurora"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type LxdCLogger struct {
+	Config *specs.LxdComposeConfig
+	Logger *zap.Logger
+	Aurora aurora.Aurora
+}
+
+var defaultLogger *LxdCLogger = nil
+
+func NewLxdCLogger(config *specs.LxdComposeConfig) *LxdCLogger {
+	return &LxdCLogger{
+		Logger: nil,
+		Aurora: aurora.NewAurora(config.GetLogging().Color),
+		Config: config,
+	}
+}
+
+func (l *LxdCLogger) GetAurora() aurora.Aurora {
+	return l.Aurora
+}
+
+func (l *LxdCLogger) SetAsDefault() {
+	defaultLogger = l
+}
+
+func GetDefaultLogger() *LxdCLogger {
+	return defaultLogger
+}
+
+func (l *LxdCLogger) InitLogger2File() error {
+	var err error
+
+	// TODO: test permission for open logfile.
+	cfg := zap.NewProductionConfig()
+	cfg.OutputPaths = []string{l.Config.GetLogging().Path}
+	cfg.Level = level2AtomicLevel(l.Config.GetLogging().Level)
+	cfg.ErrorOutputPaths = []string{}
+	if l.Config.GetLogging().JsonFormat {
+		cfg.Encoding = "json"
+	} else {
+		cfg.Encoding = "console"
+	}
+	cfg.DisableCaller = true
+	cfg.DisableStacktrace = true
+	cfg.EncoderConfig.TimeKey = "time"
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	l.Logger, err = cfg.Build()
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Error on initialize file logger: "+err.Error()+"\n")
+		return err
+	}
+
+	return nil
+}
+
+func level2Number(level string) int {
+	switch level {
+	case "error":
+		return 0
+	case "warning":
+		return 1
+	case "info":
+		return 2
+	default:
+		return 3
+	}
+}
+
+func (l *LxdCLogger) log2File(level, msg string) {
+	switch level {
+	case "error":
+		l.Logger.Error(msg)
+	case "warning":
+		l.Logger.Warn(msg)
+	case "info":
+		l.Logger.Info(msg)
+	default:
+		l.Logger.Debug(msg)
+	}
+}
+
+func level2AtomicLevel(level string) zap.AtomicLevel {
+	switch level {
+	case "error":
+		return zap.NewAtomicLevelAt(zap.ErrorLevel)
+	case "warning":
+		return zap.NewAtomicLevelAt(zap.WarnLevel)
+	case "info":
+		return zap.NewAtomicLevelAt(zap.InfoLevel)
+	default:
+		return zap.NewAtomicLevelAt(zap.DebugLevel)
+	}
+}
+
+func (l *LxdCLogger) msg(level string, withoutColor bool, msg ...interface{}) {
+	var message string
+	var confLevel, msgLevel int
+
+	if l.Config.GetGeneral().HasDebug() {
+		confLevel = 3
+	} else {
+		confLevel = level2Number(l.Config.GetLogging().Level)
+	}
+	msgLevel = level2Number(level)
+	if msgLevel > confLevel {
+		return
+	}
+
+	for _, m := range msg {
+		message += " " + fmt.Sprintf("%v", m)
+	}
+
+	var levelMsg string
+
+	if withoutColor || !l.Config.GetLogging().Color {
+		levelMsg = message
+	} else {
+		switch level {
+		case "warning":
+			levelMsg = aurora.Bold(aurora.Yellow(":construction: " + message)).BgBlack().String()
+		case "debug":
+			levelMsg = aurora.White(message).BgBlack().String()
+		case "info":
+			levelMsg = aurora.Bold(aurora.White(message)).BgBlack().String()
+		case "error":
+			levelMsg = aurora.Bold(aurora.Red(":bomb: " + message + ":fire:")).BgBlack().String()
+		}
+	}
+
+	if l.Config.GetLogging().EnableEmoji {
+		levelMsg = emoji.Sprint(levelMsg)
+	} else {
+		re := regexp.MustCompile(`[:][\w]+[:]`)
+		levelMsg = re.ReplaceAllString(levelMsg, "")
+	}
+
+	if l.Logger != nil {
+		l.log2File(level, message)
+	}
+
+	fmt.Println(levelMsg)
+}
+
+func (l *LxdCLogger) Warning(mess ...interface{}) {
+	l.msg("warning", false, mess...)
+	//if l.FatalWarns {
+	//	os.Exit(2)
+	//}
+}
+
+func (l *LxdCLogger) Debug(mess ...interface{}) {
+	l.msg("debug", false, mess...)
+}
+
+func (l *LxdCLogger) DebugC(mess ...interface{}) {
+	l.msg("debug", true, mess...)
+}
+
+func (l *LxdCLogger) Info(mess ...interface{}) {
+	l.msg("info", false, mess...)
+}
+
+func (l *LxdCLogger) InfoC(mess ...interface{}) {
+	l.msg("info", true, mess...)
+}
+
+func (l *LxdCLogger) Error(mess ...interface{}) {
+	l.msg("error", false, mess...)
+}
+
+func (l *LxdCLogger) Fatal(mess ...interface{}) {
+	l.Error(mess)
+	os.Exit(1)
+}
--- pkg/specs/config.go
+++ pkg/specs/config.go
@@ -36,12 +36,33 @@ type LxdComposeConfig struct {
 	Viper *v.Viper `yaml:"-" json:"-"`
 
 	General         LxdCGeneral `mapstructure:"general" json:"general,omitempty" yaml:"general,omitempty"`
+	Logging         LxdCLogging `mapstructure:"logging" json:"logging,omitempty" yaml:"logging,omitempty"`
 	EnvironmentDirs []string    `mapstructure:"env_dirs,omitempty" json:"env_dirs,omitempty" yaml:"env_dirs,omitempty"`
 }
 
 type LxdCGeneral struct {
 	Debug      bool   `mapstructure:"debug,omitempty" json:"debug,omitempty" yaml:"debug,omitempty"`
 	LxdConfDir string `mapstructure:"lxd_confdir,omitempty" yaml:"lxd_confdir,omitempty"`
+}
+
+type LxdCLogging struct {
+	// Path of the logfile
+	Path string `mapstructure:"path,omitempty" json:"path,omitempty" yaml:"path,omitempty"`
+	// Enable/Disable logging to file
+	EnableLogFile bool `mapstructure:"enable_logfile,omitempty" json:"enable_logfile,omitempty" yaml:"enable_logfile,omitempty"`
+	// Enable JSON format logging in file
+	JsonFormat bool `mapstructure:"json_format,omitempty" json:"json_format,omitempty" yaml:"json_format,omitempty"`
+
+	// Log level
+	Level string `mapstructure:"level,omitempty" json:"level,omitempty" yaml:"level,omitempty"`
+
+	// Enable emoji
+	EnableEmoji bool `mapstructure:"enable_emoji,omitempty" json:"enable_emoji,omitempty" yaml:"enable_emoji,omitempty"`
+	// Enable/Disable color in logging
+	Color bool `mapstructure:"color,omitempty" json:"color,omitempty" yaml:"color,omitempty"`
+
+	// Enable/Disable commands output logging
+	CmdsOutput bool `mapstructure:"cmds_output,omitempty" json:"cmds_output,omitempty" yaml:"cmds_output,omitempty"`
 }
 
 func NewLxdComposeConfig(viper *v.Viper) *LxdComposeConfig {
@@ -59,6 +80,10 @@ func (c *LxdComposeConfig) GetGeneral() *LxdCGeneral {
 
 func (c *LxdComposeConfig) GetEnvironmentDirs() []string {
 	return c.EnvironmentDirs
+}
+
+func (c *LxdComposeConfig) GetLogging() *LxdCLogging {
+	return &c.Logging
 }
 
 func (c *LxdComposeConfig) Unmarshal() error {
@@ -83,6 +108,15 @@ func (c *LxdComposeConfig) Yaml() ([]byte, error) {
 func GenDefault(viper *v.Viper) {
 	viper.SetDefault("general.debug", false)
 	viper.SetDefault("general.lxd_confdir", "")
+
+	viper.SetDefault("logging.level", "info")
+	viper.SetDefault("logging.enable_logfile", false)
+	viper.SetDefault("logging.path", "/var/log/luet.log")
+	viper.SetDefault("logging.json_format", false)
+	viper.SetDefault("logging.enable_emoji", true)
+	viper.SetDefault("logging.color", true)
+	viper.SetDefault("logging.cmds_output", true)
+
 	viper.SetDefault("env_dirs", []string{"./lxd-compose/envs"})
 }
 
