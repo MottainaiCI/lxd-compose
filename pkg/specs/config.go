796a25c924ab92aa1b9f8eecc05d4856bfd18413
Daniele Rondina <geaaru@sabayonlinux.org>
2020-08-05 08:43:23 +0200

    config: Default logs path is ./logs/lxd-compose.log

--- pkg/specs/config.go
+++ pkg/specs/config.go
@@ -112,7 +112,7 @@ func GenDefault(viper *v.Viper) {
 
 	viper.SetDefault("logging.level", "info")
 	viper.SetDefault("logging.enable_logfile", false)
-	viper.SetDefault("logging.path", "/var/log/luet.log")
+	viper.SetDefault("logging.path", "./logs/lxd-compose.log")
 	viper.SetDefault("logging.json_format", false)
 	viper.SetDefault("logging.enable_emoji", true)
 	viper.SetDefault("logging.color", true)
