6aede5f3120c6933fd760468feb9eb4d7fd5b6aa
Daniele Rondina <geaaru@sabayonlinux.org>
2021-05-27 11:58:19 +0200

    cmd/node/list: Review rendering and added --json,--search

--- cmd/node/list.go
+++ cmd/node/list.go
@@ -22,27 +22,32 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd_node
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/MottainaiCI/lxd-compose/pkg/executor"
+	"github.com/MottainaiCI/lxd-compose/pkg/helpers"
 	loader "github.com/MottainaiCI/lxd-compose/pkg/loader"
 	specs "github.com/MottainaiCI/lxd-compose/pkg/specs"
 
+	tablewriter "github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
 func NewListCommand(config *specs.LxdComposeConfig) *cobra.Command {
 	var cmd = &cobra.Command{
 		Use:   "list",
-		Short: "list of nodes.",
+		Short: "list of nodes available to the specified endpoint.",
 		Run: func(cmd *cobra.Command, args []string) {
 
 			confdir, _ := cmd.Flags().GetString("lxd-config-dir")
+			jsonOutput, _ := cmd.Flags().GetBool("json")
+			endpoint, _ := cmd.Flags().GetString("endpoint")
+			search, _ := cmd.Flags().GetString("search")
 
 			// Create Instance
 			composer := loader.NewLxdCInstance(config)
-			endpoint, _ := cmd.Flags().GetString("endpoint")
 
 			err := composer.LoadEnvironments()
 			if err != nil {
@@ -69,14 +74,55 @@ func NewListCommand(config *specs.LxdComposeConfig) *cobra.Command {
 				fmt.Println("Error on retrieve container list: " + err.Error() + "\n")
 				os.Exit(1)
 			}
-			for _, c := range list {
-				fmt.Println("- ", c)
+
+			if search != "" {
+				list = helpers.RegexEntry(search, list)
+			}
+
+			if jsonOutput {
+				data, _ := json.Marshal(list)
+				fmt.Println(string(data))
+
+			} else {
+				table := tablewriter.NewWriter(os.Stdout)
+				table.SetBorders(tablewriter.Border{
+					Left: true, Top: false, Right: true, Bottom: false,
+				})
+
+				table.SetCenterSeparator("|")
+				table.SetHeader([]string{
+					"Node Name", "Project Name", "Group Name",
+				})
+
+				for _, n := range list {
+
+					pName := ""
+					gName := ""
+
+					_, proj, group, _ := composer.GetEntitiesByNodeName(n)
+
+					if proj != nil {
+						pName = proj.GetName()
+						gName = group.GetName()
+					}
+
+					table.Append([]string{
+						n,
+						pName,
+						gName,
+					})
+
+				}
+
+				table.Render()
 			}
 		},
 	}
 
 	pflags := cmd.Flags()
 	pflags.StringP("endpoint", "e", "", "Set endpoint of the LXD connection")
+	pflags.Bool("json", false, "JSON output")
+	pflags.StringP("search", "s", "", "Regex filter to use with node name.")
 
 	return cmd
 }
