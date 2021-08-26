77ea2c37c1133cecb8c8f2584bdc5c91c775bdb2
Daniele Rondina <geaaru@sabayonlinux.org>
2021-08-26 18:55:32 +0200

    Prepare release v.0.12.3

--- cmd/root.go
+++ cmd/root.go
@@ -37,7 +37,7 @@ const (
 
 Mottainai - LXD Compose Integrator`
 
-	LXD_COMPOSE_VERSION = `0.12.2`
+	LXD_COMPOSE_VERSION = `0.12.3`
 )
 
 // Build time and commit information. This code is get from: https://github.com/mudler/luet/
