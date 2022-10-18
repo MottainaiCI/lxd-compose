f20ed57f3c4132a836cf31e3121ff83a05a5242f
Daniele Rondina <geaaru@funtoo.org>
2022-10-18 21:26:40 +0200

    Cmd/storage/available: Fix typo

--- cmd/storage/available.go
+++ cmd/storage/available.go
@@ -1,5 +1,4 @@
 /*
-
 Copyright (C) 2020-2022 Daniele Rondina <geaaru@funtoo.org>
 Credits goes also to Gogs authors, some code portions and re-implemented design
 are also coming from the Gogs project, which is using the go-macaron framework
@@ -17,7 +16,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 */
 package cmd_storage
 
@@ -72,7 +70,7 @@ func NewAvailableCommand(config *specs.LxdComposeConfig) *cobra.Command {
 
 			list, err := executor.GetStorageList()
 			if err != nil {
-				fmt.Println("Error on retrieve network list: " + err.Error() + "\n")
+				fmt.Println("Error on retrieve storage list: " + err.Error() + "\n")
 				os.Exit(1)
 			}
 
